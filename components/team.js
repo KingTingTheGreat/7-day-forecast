46fc3d856cde4be8694ed5d4cf70639f925f8b9e
Jeffrey <jting@bu.edu>
2023-12-11 14:25:04 -0500

    fixing animations

--- components/team.js
+++ components/team.js
@@ -82,6 +82,11 @@ const LightBox = styled.div`
 			--border-angle: 1turn;
 		}
 	}
+	@property --border-angle {
+		syntax: "<angle>";
+		inherits: true;
+		initial-value: 0turn;
+	}
 `;
 
 const SocialLinks = (socials) => {
